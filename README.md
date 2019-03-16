1d327e7dadef5b530e63bea0799b0b76e8c707d3
liujian <346643585@qq.com>
2019-03-16 16:01:17 +0800

    DefaultBeanFactory中存在读取bean.xml和反射创建对象两种操作，这不符合设计模式的单一职责。因此我们新增加一个XmlBeanDefinitionReader来读取bean.xml

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # small-spring
 简版spring
-##
-+ Spring是一个开放源代码的设计层面框架，它解决的是业务逻辑层和其他各层的松耦合问题，因此它将面向接口的编程思想贯穿整个系统应用。
-+ bean-v1.xml配置bean的信息
-+ BeanDefinition用于存放bean的定义
-+ BeanFactory获取bean的信息，实例化bean`
-+ BeanFactoryTest测试BeanFactory是否可用
+##BeanFactory(二)
+在Small Spring系列一：
+BeanFactory(一)中，我们用DefaultBeanFactory读取bean.xlm中的bean信息，并且也实现了BeanFactory的getBean()方法。
+但是实现的方式有些不友好，本章，我们将优化和完善BeanFactory。
+
+
+你会发现在DefaultBeanFactory中存在读取bean.xml和反射创建对象两种操作，这不符合设计模式的单一职责。
+因此我们新增加一个XmlBeanDefinitionReader来读取bean.xml。
+但解析的BeanDefinition如何交给BeanFactory处理呢？
+这里我们可以在BeanFactory接口中增加registerBeanDefinition(String beanID, BeanDefinition bd)方法，
+将XmlBeanDefinitionReader解析的BeanDefinition注册到BeanFactory中。
+(此时XmlBeanDefinitionReader中需要持有一个BeanFactory的实例)。
+类图如下: 
+https://raw.githubusercontent.com/longfeizheng/longfeizheng.github.io/master/images/spring/beanfactory_v2_1.png
---
+++ src/main/java/com/niocoder/beans/factory/support/XmlBeanDefinitionReader.java
@@ -0,0 +1,49 @@
+package com.niocoder.beans.factory.support;
+
+import com.niocoder.beans.BeanDefinition;
+import com.niocoder.beans.factory.BeanDefinitionStoreException;
+import com.niocoder.beans.factory.BeanFactory;
+import com.niocoder.util.ClassUtils;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+public class XmlBeanDefinitionReader {
+
+    private static final String ID_ATTRIBUTE = "id";
+
+    private static final String CLASS_ATTRIBUTE = "class";
+
+    BeanFactory beanFactory;
+    public XmlBeanDefinitionReader(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+
+    }
+    /**
+     * 具体解析bean.xml的方法 使用dom4j
+     *
+     * @param configFile
+     */
+    public void loadBeanDefinition(String configFile) {
+        ClassLoader cl = ClassUtils.getDefaultClassLoader();
+        try (InputStream is = cl.getResourceAsStream(configFile)) {
+            SAXReader reader = new SAXReader();
+            Document doc = reader.read(is);
+
+            Element root = doc.getRootElement();
+            Iterator<Element> elementIterator = root.elementIterator();
+            while (elementIterator.hasNext()) {
+                Element ele = elementIterator.next();
+                String id = ele.attributeValue(ID_ATTRIBUTE);
+                String beanClassName = ele.attributeValue(CLASS_ATTRIBUTE);
+                BeanDefinition bd = new GenericBeanDefinition(id, beanClassName);
+                this.beanFactory.registerBeanDefinition(id, bd);
+            }
+        } catch (Exception e) {
+            throw new BeanDefinitionStoreException("IOException parsing XML document", e);
+        }
+    }
+}
